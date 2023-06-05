b0163cbde1c044f16300fb9379cff05cf3151b6b
userfiona <fionaramaesela@gmail.com>
2023-06-05 15:26:43 -0700

    origin

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 s = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 s = s[39:66] + s[106:112] + s[:6]
 print(s)
