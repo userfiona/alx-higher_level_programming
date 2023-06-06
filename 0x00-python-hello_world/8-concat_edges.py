d4e7b3e2194638559b64d7eb57d250d5b3b717f9
userfiona <fionaramaesela@gmail.com>
2023-06-05 17:51:47 -0700

    origin

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, python with combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, python and with object-oriented programming\
+ language combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
 print(str)
