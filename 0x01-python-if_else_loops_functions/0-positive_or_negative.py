a72a4d4a5c563f4f5f2c16ece21918fe091fa760
userfiona <fionaramaesela@gmail.com>
2023-06-06 14:23:59 -0700

    j

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 
-random_number = random.randint(-98, 98)
+random_number = random.randint(-10, 10)
 
 if random_number > 0:
     print(f"{random_number} is positive")
