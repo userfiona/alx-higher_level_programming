d471cf0425fee3297ca38694728c17402c84456b
userfiona <fionaramaesela@gmail.com>
2023-06-06 16:42:34 -0700

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
 for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        if num2 != 9:
+    for num2 in range(num1, 10):
+        if num1 != num2:
             print("{:02d}, ".format(num1 * 10 + num2), end="")
-        else:
-            print("{:02d}".format(num1 * 10 + num2))
-
+print() 
