8be22591d5247f8fe138991df58b7a2120d74b30
userfiona <fionaramaesela@gmail.com>
2023-06-06 16:13:31 -0700

    j

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{} = {:02x}".format(i, i), end=", ")
-print("99 = 63")
+for i in range(0, 99):
+    print("{} = {:02x}".format(i, i), end=', ')
+print("{:02d}".format(99))
