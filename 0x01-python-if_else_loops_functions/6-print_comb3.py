afc306287f3a315ca2fe3a7c371470075f4a7bed
userfiona <fionaramaesela@gmail.com>
2023-06-07 13:17:53 -0700

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num in range(10):
+for num in range(9):
     for next_num in range(num + 1, 10):
         print("{:d}{:d}".format(num, next_num), end=", ")
-print()
+print("99")
