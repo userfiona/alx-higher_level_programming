c279ef375e0f3ce99ef3e7ef36639aede4ebdd6e
userfiona <fionaramaesela@gmail.com>
2023-06-07 13:23:37 -0700

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(10):
     for next_num in range(num + 1, 10):
-        print("{:02d}, ".format(num * 10 + next_num), end='')
-print("99")
+        print("{:d}{:d}".format(num, next_num), end=", ")
+print()
