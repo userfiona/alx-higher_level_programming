e1e396f1cdfb181d43bc080d82b71582bfda93da
userfiona <fionaramaesela@gmail.com>
2023-06-07 01:43:25 -0700

    j

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit, end="")
-    return last_digit
+last_digit = print_last_digit(12345)  # Output: 5
+print(last_digit)  # Output: 5
