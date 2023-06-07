64358f11878186b731d09597b07ae6a0fdebbdc9
userfiona <fionaramaesela@gmail.com>
2023-06-07 13:46:29 -0700

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num in range(10):
+for num in range(1, 10):
     for next_num in range(num + 1, 10):
-        print("{:d}{:d}, ".format(num, next_num), end="")
-print()
+        print("{:02d}, {:02d}".format(num, next_num), end=", ")
+print("{:02d}".format(98))
