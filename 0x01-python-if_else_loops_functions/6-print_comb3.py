e6bfc3cbc85249c68b6c40dd1541c8ea96b72e87
userfiona <fionaramaesela@gmail.com>
2023-06-07 13:30:30 -0700

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
+User
 #!/usr/bin/python3
 for num in range(10):
-    for next_num in range(num, 10):
-        if num != next_num:
-            print("{:d}{:d}".format(num, next_num), end=", ")
+    for next_num in range(num + 1, 10):
+        print("{:d}{:d}".format(num, next_num), end=", ")
 print()
