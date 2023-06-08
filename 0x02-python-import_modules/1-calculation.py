83a051ea74fb1f2270f4beb36797c2d19507670d
userfiona <fionaramaesela@gmail.com>
2023-06-08 15:20:05 -0700

    upgrade

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,14 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    # Perform calculations
+    sum_result = add(a, b)
+    difference_result = sub(a, b)
+    product_result = mul(a, b)
+    quotient_result = div(a, b)
+
+    # Print the results
+    print("Sum: {} + {} = {}".format(a, b, sum_result))
+    print("Difference: {} - {} = {}".format(a, b, difference_result))
+    print("Product: {} * {} = {}".format(a, b, product_result))
+    print("Quotient: {} / {} = {:.2f}".format(a, b, quotient_result))
