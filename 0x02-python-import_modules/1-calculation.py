04e0b76b34550db68b435e4f277b12bb9e6f92d6
userfiona <fionaramaesela@gmail.com>
2023-06-08 15:35:17 -0700

    upgrade

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+def div(a, b):
+    """My division function
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
+    Args:
+        a: first integer
+        b: second integer
 
-    # Perform calculations
-    sum_result = add(a, b)
-    difference_result = sub(a, b)
-    product_result = mul(a, b)
-    quotient_result = div(a, b)
-
-    # Print the results
-    print("Sum: {:d} + {:d} = {:d}".format(a, b, sum_result))
-    print("Difference: {:d} - {:d} = {:d}".format(a, b, difference_result))
-    print("Product: {:d} * {:d} = {:d}".format(a, b, product_result))
-    print("Quotient: {:d} / {:d} = {:d}".format(a, b, quotient_result))
+    Returns:
+        The return value. a / b
+    """
+    return "{:.1f}".format(a / b)
