a231dfe674d2321993c765892e4a9be599aa459c
userfiona <fionaramaesela@gmail.com>
2023-06-08 01:49:58 -0700

    j

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for char_code in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(char_code if char_code % 2 == 0 else char_code - 32), end='')
+    if char_code % 2 == 0:
+        print(chr(char_code), end='')
+    else:
+        print(chr(char_code - 32), end='')
 
 print()
