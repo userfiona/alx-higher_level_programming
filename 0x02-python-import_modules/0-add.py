41a6a39fb5c69eff81190ba215d2594814269d0d
userfiona <fionaramaesela@gmail.com>
2023-06-08 10:55:48 -0700

    upgrade

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
 a = 1
 b = 2
 
 result = add(a, b)
 
-print("{} + {} = {}".format(a, b, sum))
+print("{} + {} = {}".format(a, b, result))
