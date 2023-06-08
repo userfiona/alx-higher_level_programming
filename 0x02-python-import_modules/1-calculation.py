ee2d072a1e0d91d6bf71a397d29de538f3698063
userfiona <fionaramaesela@gmail.com>
2023-06-08 15:09:33 -0700

    upgrade

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {2:d}".format(a, b, div(a, b)))
+    print("{:d} / {:d} = {2f:d}".format(a, b, div(a, b)))
