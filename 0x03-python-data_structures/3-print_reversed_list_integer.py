fcc961e816ba0c01f497469b25e1d94288061b39
userfiona <fionaramaesela@gmail.com>
2023-06-10 04:56:44 -0700

    code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 # 3-print_reversed_list_integer.py
 
 def print_reversed_list_integer(my_list=[]):
-    """Prints the integers in a list in reverse order, with one integer per line."""
+    """Print a list of integers... in reverse!"""
     if not my_list:
         return None
     my_list.reverse()
