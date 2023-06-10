95dca9c8d448642f5a96909a9aee1bcbe5cb657c
userfiona <fionaramaesela@gmail.com>
2023-06-10 03:18:39 -0700

    code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,6 +8,9 @@ def replace_in_list(my_list, idx, element):
         my_list: The list in which the element needs to be replaced.
         idx: The index position at which the element should be replaced.
         element: The new element to replace the existing one.
+
+        Returns:
+        The updated list after replacing the element, or the original list if the index is out of range.
          """
 
     if idx < 0 or idx >= len(my_list):
