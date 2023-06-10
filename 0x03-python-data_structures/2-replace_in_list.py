8d01ab52538fdd1cf56322959f6ce62103abc84e
userfiona <fionaramaesela@gmail.com>
2023-06-10 04:37:56 -0700

    push

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,17 +2,7 @@
 # 2-replace_in_list.py
 
 def replace_in_list(my_list, idx, element):
-    """
-    Replaces an element in a list at a specific position.
-
-    Args:
-        my_list: The list in which the element needs to be replaced.
-        idx: The index position at which the element should be replaced.
-        element: The new element to replace the existing one.
-
-    Returns:
-        The updated list after replacing the element, or the original list if the index is out of range.
-    """
+    """ Replaces an element in a list at a specific position."""
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
