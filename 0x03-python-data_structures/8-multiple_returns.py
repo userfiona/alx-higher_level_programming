2d15cda874c78ae8fd23dfad60b00497c7caf9eb
userfiona <fionaramaesela@gmail.com>
2023-06-10 05:07:10 -0700

    code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """Write a function that returns a tuple with the length of a string and its first character."""
+    """More returns!"""
     if len(sentence) == 0:
         result_tuple = len(sentence), None
         return result_tuple
