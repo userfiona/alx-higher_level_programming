f169129efeb22a6e712ba8474f92603308752e81
userfiona <fionaramaesela@gmail.com>
2023-07-12 17:20:46 -0700

    status

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ Module of a function that writes to a text file and returns the number of charac
 
 def write_file(filename="", text=""):
     """Write to a file"""
+
     with open(filename, 'w', encoding="utf-8") as file:
         return file.write(text)
