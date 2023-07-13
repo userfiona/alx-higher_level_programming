6905657c9f5102551f42c5fd928f874fdb7b787b
userfiona <fionaramaesela@gmail.com>
2023-07-12 18:22:38 -0700

    status

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""
-Module of a function that writes to a text file and returns the number of characters written.
-"""
+"""Append to a file"""
 
 
 def write_file(filename="", text=""):
-    """Write to a file and return the number of characters written.
-
-    Args:
-        filename (str): Name of the file to write to.
-        text (str): Text to be written.
-
-    Returns:
-        int: Number of characters written to the file.
-    """
+    """Append to a file"""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
