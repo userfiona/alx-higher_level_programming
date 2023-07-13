65e064c0a99d1ce4c60b9b70295944cc2e833503
userfiona <fionaramaesela@gmail.com>
2023-07-12 18:17:09 -0700

    status

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-Module to add all arguments to a Python list and then save to a file.
+   Module to add all arguments to a python list,
+     then save to a file.
 """
 
+
 import sys
-import json
+
 
 def main():
     """
-    Main function to add arguments to a list and save to a file.
+       main - main body
     """
-    filename = "add_item.json"
-    try:
-        with open(filename, 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        data = []
-        
-    data.extend(sys.argv[1:])
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
 
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+    try:
+        s = load_json("add_item.json")
+    except FileNotFoundError:
+        s = []
+    s.extend(sys.argv[1:])
+    save_json(s, "add_item.json")
+
 
 if __name__ == "__main__":
     main()
