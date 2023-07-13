8ebbb70553df1d727839472d0c1845fcc5852cef
userfiona <fionaramaesela@gmail.com>
2023-07-12 19:32:16 -0700

    status

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""2.Exact same object"""
+"""2. Exact same object"""
 
 def is_same_class(obj, a_class):
-    """2. Exact same object""""
+    """2. Exact same object"""
     return type(obj) == a_class
