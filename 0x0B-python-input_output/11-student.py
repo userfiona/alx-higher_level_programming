1dc21a415bf7d6c1ff4a3ae3b4a21c21a2232892
userfiona <fionaramaesela@gmail.com>
2023-07-12 17:33:32 -0700

    status

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python3
-"""
-Defines a class Student.
-"""
+"""Student to disk and reload"""
 
 
 class Student:
-    """
-    Represents a student.
-    """
+    """Student to disk and reload """
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new Student instance.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Student to disk and reload"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of the Student instance.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to include in the dictionary.
-
-        Returns:
-            dict: A dictionary representation of the Student instance.
-        """
+        """Student to disk and reload"""
         if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
 
     def reload_from_json(self, json_data):
-        """
-        Replaces all attributes of the Student instance with new values from a dictionary.
-
-        Args:
-            json_data (dict): The key/value pairs to replace attributes with.
-        """
+        """Student to disk and reload"""
         for key, value in json_data.items():
             setattr(self, key, value)
