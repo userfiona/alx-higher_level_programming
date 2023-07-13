15f8adbede6aa7bc8984a48f84f17173d2df0772
userfiona <fionaramaesela@gmail.com>
2023-07-12 19:35:21 -0700

    status

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-MyList module
+contains the MyList class
 """
 
 
 class MyList(list):
-    """
-    MyList class, a child of list
-    """
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
-        """
-        Print the list in sorted order.
-        """
+        """prints the sorted list"""
         print(sorted(self))
