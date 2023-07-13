81e0e57a7cc9c0d7250a6a9fd8205b5f7aced56e
userfiona <fionaramaesela@gmail.com>
2023-07-12 18:52:11 -0700

    status

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-Check if an object is an instance of a class that inherited
-from a specified class.
-"""
+"""Check if object is an instance of a class that
+inherited from specified class"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if the object is an instance of a class that
-    inherited from the specified class, otherwise return False.
-    """
-    if issubclass(obj.__class__, a_class) and obj.__class__ is not a_class:
-        return True
-    else:
-        return False
+    """Return True if object is an instance of a class that
+    inherited from specified class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
