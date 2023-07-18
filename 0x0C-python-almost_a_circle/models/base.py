5f3abef37224226c4f5940e55690359824126548
userfiona <fionaramaesela@gmail.com>
2023-07-18 13:02:29 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,18 +46,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-         """
-    Create a new instance of the class using a dictionary of attributes.
-
-    Args:
-        **dictionary: Keyword arguments representing the attributes of the instance.
-
-    Returns:
-        object: New instance of the class with the provided attributes.
-
-    Raises:
-        None
-    """
+         """do string"""
         if cls.__name__ == "Rectangle":
             dummy_instance = cls(1, 1)
         elif cls.__name__ == "Square":
