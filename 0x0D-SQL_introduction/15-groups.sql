b591dac0de43ab93a3dd44cf29c632eb4b0af6c9
userfiona <fionaramaesela@gmail.com>
2023-08-15 06:44:24 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,6 +2,6 @@
 
 -- Prints the number of records within a group in the table in the database
 SELECT `score`, COUNT(`score`) AS `number`
-FROM `hbtn_0c_0`.`second_table`
+FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
