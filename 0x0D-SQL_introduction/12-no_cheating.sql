7ea170a53937cf8b4ab3410ef514715f0a4b178d
userfiona <fionaramaesela@gmail.com>
2023-08-15 06:37:06 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -3,4 +3,4 @@
 -- Updates the score of Bob to 10 in the table second_table.
 -- You are not allowed to use Bob’s id value, only the name field
 
-UPDATE `hbtn_0c_0`.`second_table` SET `score` = 10 WHERE `name` = 'Bob';
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
