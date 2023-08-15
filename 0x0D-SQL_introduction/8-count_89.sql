e66f4b61450a9d011567faacb2e2b47f0c379e2a
userfiona <fionaramaesela@gmail.com>
2023-08-15 06:27:39 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -3,4 +3,4 @@
 -- Displays the number of records with id = 89 in the table first_table
 -- The database name will be passed as an argument of the mysql command
 
-SELECT COUNT(*) FROM `hbtn_0c_0`.`first_table` WHERE `id` = 89;
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89;
