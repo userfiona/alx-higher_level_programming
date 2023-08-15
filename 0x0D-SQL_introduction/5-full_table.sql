a805ef47a35c3e394b7eabc45b612867781ac253
userfiona <fionaramaesela@gmail.com>
2023-08-15 05:54:38 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -3,5 +3,5 @@
 -- Full description of the table first_table
 -- Display table info without using DESCRIBE or EXPLAIN
 
-USE hbtn_0c_0;
-SHOW CREATE TABLE first_table;
+USE hbtn_0c_0
+SHOW CREATE TABLE first_table
