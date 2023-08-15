c95bdf344c8016485733243716cd24ec9869224e
userfiona <fionaramaesela@gmail.com>
2023-08-15 07:03:29 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- 100-move_to_utf8.sql
 
--- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- converts database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table
