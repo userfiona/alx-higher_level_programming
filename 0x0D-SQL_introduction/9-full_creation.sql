3d53c85f48f3204d03540f3426054fd87fa98cc8
userfiona <fionaramaesela@gmail.com>
2023-08-15 06:31:03 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `hbtn_0c_0`.`second_table` (
 	`score` INT
 );
 
-INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
-INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
-INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
-INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
