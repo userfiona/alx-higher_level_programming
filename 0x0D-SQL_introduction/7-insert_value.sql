c58dfa56559dbce00e1f234d9dfa318007b4e557
userfiona <fionaramaesela@gmail.com>
2023-08-15 06:21:58 -0700

    and that is how we wright sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- 7-insert_value.sql
 
 -- Inserts a new row into the table first_table.
-INSERT INTO `hbtn_0c_0`.`first_table` (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO (`id`, `name`) VALUES (89, 'Best School');
