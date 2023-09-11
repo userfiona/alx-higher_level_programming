5cb7ec267f25e629766a1a94785ae5e79316f4dc
userfiona <fionaramaesela@gmail.com>
2023-09-11 12:46:55 -0700

    please just work

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,11 @@
 const numOccurrences = parseInt(process.argv[2]);
 
 if (!isNaN(numOccurrences) && numOccurrences > 0) {
+  let output = '';
   for (let i = 0; i < numOccurrences; i++) {
-    console.log('C is fun');
+    output += 'C is fun\n';
   }
+  console.log(output.trim()); // Print the accumulated output with line breaks trimmed
 } else {
   console.log('Missing number of occurrences');
 }
