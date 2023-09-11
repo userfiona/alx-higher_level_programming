ae0c769ea258dd3b08bb1d11cedeec6c57399bb3
userfiona <fionaramaesela@gmail.com>
2023-09-11 12:41:40 -0700

    please just work

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3]) {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else {
-  console.log('Not enough arguments. Usage: ./script.js arg1 arg2');
+if (process.argv.length !== 4) {
+  console.error('Error: Please provide exactly 2 arguments.');
+  console.log('Usage: ./script.js arg1 arg2');
+  process.exit(1); // Exit the script with an error code
 }
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (!arg1 || !arg2) {
+  console.error('Error: Both arguments must be non-empty.');
+  console.log('Usage: ./script.js arg1 arg2');
+  process.exit(1); // Exit the script with an error code
+}
+
+console.log(`${arg1} is ${arg2}`);
