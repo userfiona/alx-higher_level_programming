e161865d7f14dc182bb7cbd2f3a724c5d296348d
userfiona <fionaramaesela@gmail.com>
2023-09-11 11:44:13 -0700

    please just work

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-// Define a constant variable called myVar
+// Define a constant variable called myVar;
 const myVar = 'JavaScript is amazing';
 
-// Use console.log to print the value of myVar
+// Use console.log to print the value of myVar;
 console.log(myVar);
