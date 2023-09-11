46ed72763d168c7e59fed0a3256f3d9086d79477
userfiona <fionaramaesela@gmail.com>
2023-09-11 13:37:17 -0700

    please just work

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 const x = Math.floor(Number(process.argv[2]));
-
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  const output = Array(x).fill('C is fun').join('\n');
-  console.log(output);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
