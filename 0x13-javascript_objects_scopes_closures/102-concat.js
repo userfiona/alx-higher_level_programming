70e5a3c6af795f0459d881081f4cb0511b033c96
userfiona <fionaramaesela@gmail.com>
2023-09-13 13:13:09 -0700

    always learning

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
-const fs = require('fs').promises;
+// reads two text files and concatenates contents into new file
+const fs = require('fs');
+// fs is standard but needs import
 
-async function concatFiles() {
-  try {
-    const file1Data = await fs.readFile(process.argv[2], 'utf-8');
-    const file2Data = await fs.readFile(process.argv[3], 'utf-8');
-    const output = file1Data + file2Data;
-    await fs.writeFile(process.argv[4], output);
-    console.log('Files concatenated successfully.');
-  } catch (err) {
-    console.error('An error occurred:', err);
-  }
-}
-
-concatFiles();
+// each `readFile` and `writeFile` call is asynchronous
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) throw err;
+  // data is bytestream before encoding
+  let output = data;
+  fs.readFile(process.argv[3], (err, data) => {
+    if (err) throw err;
+    output += data;
+    fs.writeFile(process.argv[4], output, (err) => {
+      if (err) throw err;
+    });
+  });
+});
