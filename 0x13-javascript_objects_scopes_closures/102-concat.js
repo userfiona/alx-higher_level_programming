c6fdf3f55558a429f5a90574f133971db2cfa1f7
userfiona <fionaramaesela@gmail.com>
2023-09-13 13:40:11 -0700

    always learning

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,31 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function concatenateFiles() {
-  fs.readFile(process.argv[2], 'utf8', (err, first) => {
-    if (err) {
-      console.error('Error reading the first file:', err);
-      return;
-    }
-    
-    fs.readFile(process.argv[3], 'utf8', (err, second) => {
-      if (err) {
-        console.error('Error reading the second file:', err);
-        return;
-      }
-
-      const combined = first + second;
-
-      fs.writeFile(process.argv[4], combined, (err) => {
-        if (err) {
-          console.error('Error writing to the destination file:', err);
-          return;
-        }
-        
-        console.log('Files concatenated successfully.');
-      });
-    });
-  });
-}
-
-concatenateFiles();
+const first = fs.readFileSync(process.argv[2], 'utf8');
+const second = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], first + second);
