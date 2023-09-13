84429d82deb3203d59895d23471760cd0a2592fe
userfiona <fionaramaesela@gmail.com>
2023-09-13 12:59:58 -0700

    always learning

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,13 @@
 
 module.exports = class Rectangle {
   constructor(w, h) {
-    this.width = w > 0 ? w : undefined;
-    this.height = h > 0 ? h : undefined;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is not a positive integer, set width and height to undefined
+      this.width = undefined;
+      this.height = undefined;
+    }
   }
 };
