06ddea50a1e0f6789c148e987372cf87c1f23c17
userfiona <fionaramaesela@gmail.com>
2023-09-20 05:51:23 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-Script that prints all City objects from the database
-"""
-from model_city import City
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-
-if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
-    Base.metadata.create_all(engine)
-
-    city = session.query(State, City).join(City).order_by(City.id)
-    for state, city in city:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    # Close session
-    session.close()
-=======
-Module that defines the City class.
+This module defines the City class.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
@@ -33,11 +8,10 @@ from model_state import Base
 
 class City(Base):
     """
-    City class definition.
+    City class representation.
     """
-
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
