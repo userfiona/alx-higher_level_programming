d3bcb817bfe9ac420171ee221262e5ac74f64d1e
userfiona <fionaramaesela@gmail.com>
2023-09-20 10:13:40 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,22 +1,28 @@
--- Create states table in hbtn_0e_101_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_101_usa;
-USE hbtn_0e_101_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+'''
+Lists all State objects, and corresponding City objects
+'''
 
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+        for city in state.cities:
+            print('\t{}: {}'.format(city.id, city.name))
+
+    session.close()
