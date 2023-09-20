e836dec569b518881788e6c42e7260c006cbbc55
userfiona <fionaramaesela@gmail.com>
2023-09-20 04:21:34 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Script that lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-from sys import argv
-
-# The code should not be executed when imported
-if __name__ == '__main__':
-
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
-=======
 
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    Username, password, and database names are given as user args
+A script that lists all states from the database hbtn_0e_0_usa
+Username, password, and database names are given as user args
 """
 
 import sys
@@ -71,4 +47,3 @@ if __name__ == '__main__':
         # Close the cursor and the database connection in a finally block
         cursor.close()
         db.close()
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
