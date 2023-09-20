7472e0ec8665435c33bcbf7270187b323c1922e5
userfiona <fionaramaesela@gmail.com>
2023-09-20 06:05:26 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,55 +1,22 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-Script that creates the `State` “California” with the
-`City` “San Francisco” from the database `hbtn_0e_100_usa`.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+Contains the class definition of a State and an instance
+Base = declarative_base()
 """
 
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
-from relationship_state import Base, State
-from relationship_city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    newState = State(name="California")
-    newState.cities.append(City(name="San Francisco"))
-
-    session.add(newState)
-    session.commit()
-=======
-Module that defines the City class.
-"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
-
-class City(Base):
+class State(Base):
     """
-    City class definition.
+    State class with attributes:
+        - id (integer, unique, auto-generated)
+        - name (string, 128 characters max, cannot be null)
     """
-
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
