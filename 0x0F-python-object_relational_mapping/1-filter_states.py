03f7d846792f0dfb950c8185b719da003cda10c5
userfiona <fionaramaesela@gmail.com>
2023-09-20 04:29:39 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,59 +1,29 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""script that lists all states with a name starting with N (upper N) from
-the database hbtn_0e_0_usa"""
-import MySQLdb
-from sys import argv
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    cur.close()
-    db.close()
-=======
-
 """
 Script that lists all states with a name starting with N (upper N)
 from the database
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # Check if all three arguments are provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
+    # Make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    try:
-        # Establish a connection to the database
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
+    # Create a cursor for database operations
+    cur = db.cursor()
 
-        # Create a cursor
-        cur = db.cursor()
+    # Execute a SQL query to select states with names starting with 'N'
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-        # Execute the query to select states starting with 'N' and order by id
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    # Fetch all rows that match the query
+    rows = cur.fetchall()
 
-        # Fetch all rows and print them
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+    # Iterate through the results and print them
+    for row in rows:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        # Clean up process
-        cur.close()
-        db.close()
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+    # Clean up process by closing cursor and database connection
+    cur.close()
+    db.close()
