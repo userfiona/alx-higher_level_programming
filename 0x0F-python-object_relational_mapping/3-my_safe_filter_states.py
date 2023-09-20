8837d7293a3e0d92b255f1c9346709c7acaeac48
userfiona <fionaramaesela@gmail.com>
2023-09-20 04:46:05 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Lists states"""
-=======
-
-"""
-Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument, safe from MySQL
-injections
-"""
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
+
+    # Execute a parameterized query to select all rows where the name matches the argument
     cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
                 (argv[4], ))
+
     query_rows = cur.fetchall()
+    
+    # Iterate through the results and print them
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
-=======
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                (argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
