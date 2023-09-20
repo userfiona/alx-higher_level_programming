80298328b97438418a328413961483a0fd1dc6fc
userfiona <fionaramaesela@gmail.com>
2023-09-20 05:08:23 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""0x0F. Python - Object-relational mapping - task 7. All states via SQLAlchemy
-"""
+"""Lists all State objects from the database hbtn_0e_6_usa"""
 
 if __name__ == '__main__':
     from sys import argv
@@ -16,31 +14,16 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
                            '{}'.format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
 
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
-=======
-
-"""
-Lists all State objects from the database hbtn_0e_6_usa
-"""
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-
-if __name__ == '__main__':
-    URL = 'mysql+mysqldb://{}:{}@localhost/{}'
-    s1, s2, s3 = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(URL.format(s1, s2, s3), pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    # Query all State objects and order by State.id in ascending order
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
         print("{}: {}".format(state.id, state.name))
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+
+    session.close()
