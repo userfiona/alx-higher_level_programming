8aae60eff7365cb1e628ae1d298ac712b92417ac
userfiona <fionaramaesela@gmail.com>
2023-09-20 10:02:19 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import create_engine
-import sys
-"""
-    Module that performs MySQL query through MySQLAlchemy.
+"""0x0F. Python - Object-relational mapping - task 17. From city
 """
 
+if __name__ == '__main__':
+    from sys import argv, exit
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from relationship_state import Base, State
+    from relationship_city import City
 
-if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                                                            sys.argv[1],
-                                                            sys.argv[2],
-                                                            sys.argv[3])
+    if len(argv) != 4:
+        exit('Use: 102-relationship_cities_states_list.py <mysql username> '
+             '<mysql password> <database name>')
 
-    engine = create_engine(db_uri, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    Base.metadata.create_all(engine)  # creates decprecated warning 1681
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for a_city in session.query(City).order_by(City.id):
-
-        print("{}: {} -> {}".format(a_city.id, a_city.name,
-                  a_city.state.name))
-
+    for city, state in session.query(City, State).filter(
+            City.state_id == State.id).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
     session.close()
