3b263b708cf47bdd08c31a5609a9465bf8cbc256
userfiona <fionaramaesela@gmail.com>
2023-09-20 09:49:32 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-'''
-Prints all City objects
-'''
+"""Module to define the City class"""
 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-from model_city import City
-
-
-if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
-
-    elements = session.query(State, City).join(City).order_by(City.id).all()
-    for state, city in elements:
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
-    session.close()
+class City(Base):
+    """City class that inherits from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
