e97afb37a97c0837b890fab884131d9013f082fe
userfiona <fionaramaesela@gmail.com>
2023-09-20 10:25:26 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Lists all City objects with respective State
-Takes three arguments
-    mysql username
-    mysql password
-    database name
-Connects to host localhost and default port (3306)
+"""0x0F. Python - Object-relational mapping - task 17. From city
 """
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    from sys import argv, exit
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm import Session
     from relationship_state import Base, State
     from relationship_city import City
-    from sys import argv
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    session = Session(bind=engine)
+
+    if len(argv) != 4:
+        exit('Use: 102-relationship_cities_states_list.py <mysql username> '
+             '<mysql password> <database name>')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    Base.metadata.create_all(engine)  # creates decprecated warning 1681
+
     for city, state in session.query(City, State).filter(
-            State.id == City.state_id).order_by(City.id).all():
-        print("{:d}: {} -> {}".format(city.id, city.name, state.name))
+            City.state_id == State.id).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
     session.close()
