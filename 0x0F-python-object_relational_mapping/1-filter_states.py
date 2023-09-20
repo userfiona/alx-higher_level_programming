0a4cfd8f74530d32a9d8b2dcf1acac5d81785678
userfiona <fionaramaesela@gmail.com>
2023-09-20 06:23:24 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,22 +8,17 @@ from sys import argv
 
 if __name__ == '__main__':
     # Make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    with MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3]) as db:
+        # Create a cursor for database operations
+        cur = db.cursor()
 
-    # Create a cursor for database operations
-    cur = db.cursor()
+        # Execute a SQL query to select states with names starting with 'N'
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-    # Execute a SQL query to select states with names starting with 'N'
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+        # Fetch all rows that match the query
+        rows = cur.fetchall()
 
-    # Fetch all rows that match the query
-    rows = cur.fetchall()
-
-    # Iterate through the results and print them
-    for row in rows:
-        print(row)
-
-    # Clean up process by closing cursor and database connection
-    cur.close()
-    db.close()
+        # Iterate through the results and print them
+        for row in rows:
+            print(row)
