2d4fa66db1af2ad2ce6d5bb14057895bc52f664a
userfiona <fionaramaesela@gmail.com>
2023-09-20 06:07:36 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,22 +1,6 @@
-#!/usr/bin/python3
-"""
-Contains the class definition of a State and an instance
-Base = declarative_base()
-"""
+-- Create the database hbtn_0e_100_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
+USE hbtn_0e_100_usa;
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class State(Base):
-    """
-    State class with attributes:
-        - id (integer, unique, auto-generated)
-        - name (string, 128 characters max, cannot be null)
-    """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+SELECT * FROM states;
+SELECT * FROM cities;
