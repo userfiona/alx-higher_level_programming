a30da0d160d933ce320fbc48f14004d15e607960
userfiona <fionaramaesela@gmail.com>
2023-09-20 08:36:11 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,13 +10,14 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == '__main__':
-    URL = 'mysql+mysqldb://{}:{}@localhost/{}'
-    s1, s2, s3 = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(URL.format(s1, s2, s3), pool_pre_ping=True)
+    # Define the URL in multiple lines for better readability
+    URL = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+    engine = create_engine(URL, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
     # Query State objects and filter those containing the letter "a"
     states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
 
