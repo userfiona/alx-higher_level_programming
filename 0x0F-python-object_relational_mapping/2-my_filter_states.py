1f31c863842b63a59f3cc30cbdf1f85712190a66
userfiona <fionaramaesela@gmail.com>
2023-09-20 04:38:52 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,60 +1,27 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
-=======
-
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
-
-import MySQLdb
-import sys
-
-if __name__ == "__main__":
-    # Check if all four arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        # Establish a connection to the database
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-        # Create a cursor
-        cur = db.cursor()
-
-        # Use a prepared statement to safely insert the state name
-        state_name = sys.argv[4]
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
-        cur.execute(query, (state_name,))
-
-        # Fetch all rows and print them
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        # Clean up process
-        cur.close()
-        db.close()
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+    # Establish a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    
+    # Create a cursor for database operations
+    cur = db.cursor()
+    
+    # Execute a SQL query to select all rows from the "states" table where the name matches the argument
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
+    
+    # Fetch all rows that match the query
+    rows = cur.fetchall()
+    
+    # Iterate through the results and print them
+    for row in rows:
+        print(row)
+    
+    # Close cursor and database connection
+    cur.close()
+    db.close()
