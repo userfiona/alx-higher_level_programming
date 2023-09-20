364e421ae5b02fc8ae39e9e76e9bd6f3da54d936
userfiona <fionaramaesela@gmail.com>
2023-09-20 05:42:31 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Script that changes the name of a State
-object from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""
+Script that changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
 
-
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    res = session.query(State).filter(State.id == 2) \
-                 .update({"name": "New Mexico"}, synchronize_session='fetch')
-=======
-"""Script that changes the name of a State object from the
-database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the database tables if they don't exist yet
     Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(id=2).first()
-    state.name = 'New Mexico'
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
-    session.commit()
+
+    # Query the State object with id = 2
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        # Update the name of the State object to "New Mexico"
+        state_to_update.name = "New Mexico"
+        
+        # Commit the transaction to save the changes
+        session.commit()
+    else:
+        print("State with id 2 not found")
+
+    # Close the session
     session.close()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
-Script that deletes all State objects with a name containing
-the letter a from the database
-Using module SQLAlchemy
+Script that deletes all State objects with a name
+containing letter a from the database hbtn_0e_6_usa
 """
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-
-if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
-    Base.metadata.create_all(engine)
-    state_del = session.query(State).filter(State.name.like('%a%')).all()
-    for delete in state_del:
-        session.delete(delete)
-    # commit and close session
-    session.commit()
-    session.close()
-=======
-# Deletes all State objects with a name containing
-# the letter 'a' from the database hbtn_0e_6_usa.
-# Usage: ./13-model_state_delete_a.py <mysql username> /
-#                                     <mysql password> /
-#                                     <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the database tables if they don't exist yet
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-    
-    session.commit()
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+    # Delete all State objects with a name containing the letter "a"
+    deleted_states = session.query(State
