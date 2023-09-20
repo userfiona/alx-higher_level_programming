a0064a68ade17a1d2c599b36e78d0315cb7ce4c8
userfiona <fionaramaesela@gmail.com>
2023-09-20 05:30:28 -0700

    my sql query

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,44 +1,35 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Script that adds the State object "Louisiana"
-to the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""
+Script that adds the State object "Louisiana"
+to the database hbtn_0e_6_usa
+"""
 
-
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    s1 = State(name="Louisiana")
-    session.add(s1)
-    session.commit()
-    print(f"{s1.id}")
-    session.close()
-=======
-# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-# Usage: ./11-model_state_insert.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine) 
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the database tables if they don't exist yet
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
+    # Create a new State object and add it to the session
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    # Commit the transaction to save the new State object to the database
     session.commit()
-    print(louisiana.id)
->>>>>>> 4af1a2f9cddcdbd0e8ccc7cb2f6b9ac828f01653
+
+    # Print the new State's id after creation
+    print(new_state.id)
+
+    # Close the session
+    session.close()
