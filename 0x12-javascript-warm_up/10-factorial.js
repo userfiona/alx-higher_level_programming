f4873951a163bcc34f74e015ae6e18e7cb7e161b
userfiona <fionaramaesela@gmail.com>
2023-09-23 15:51:29 -0700

    please just work

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,6 @@
- #!/usr/bin/node
+#!/usr/bin/node
 
 function factorial(n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  return n == = 0 | | isNaN(n) ? 1: n * factorial(n - 1);
+  return n == = 0 | | isNaN(n) ? 1: n * factorial(n - 1);
 }
