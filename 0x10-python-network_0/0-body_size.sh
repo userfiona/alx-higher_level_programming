11511b7d4cc39757d9babcac5d7192d2847a5abe
userfiona <fionaramaesela@gmail.com>
2023-09-28 05:59:06 -0700

    networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+
+# Check if a URL argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send a request to the URL using curl and save the response in a temporary file
+response_file=$(mktemp)
+curl -s "$1" > "$response_file"
+
+# Check if the request was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to retrieve the URL."
+  rm -f "$response_file"
+  exit 1
+fi
+
+# Get the size of the response body in bytes
+body_size=$(wc -c < "$response_file")
+
+# Display the size of the body in bytes
+echo "Size of the body of the response: $body_size bytes"
+
+# Clean up the temporary file
+rm -f "$response_file"
