17637f7f5611cb6fc8a16df01bdd3e22a4d2fcf9
userfiona <fionaramaesela@gmail.com>
2023-09-28 07:35:06 -0700

    networking

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-# Send a POST request to a URL and display the response body
-
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# URL encode the data
-data="email=$(urlencode "$email")&subject=$(urlencode "$subject")"
-
-# Send the POST request with the encoded data and display the response body
-curl -s "$url" -X POST -d "$data"
-
-# Function to URL encode data
-urlencode() {
-  local data
-  data=$(curl -Gso /dev/null -w %{url_effective} --data-urlencode "$1" "" 2>/dev/null)
-  echo "${data##/?}"
-}
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
