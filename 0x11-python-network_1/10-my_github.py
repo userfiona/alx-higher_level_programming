64e1440fde820c4b7fc2c34ea3b1d63bed0a0c2c
userfiona <fionaramaesela@gmail.com>
2023-09-29 14:53:40 -0700

    add network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """
-Takes my Github credentials (username & password)
-and uses the Github API to display my ID.
+Takes my GitHub credentials (username & personal access token)
+and uses the GitHub API to display my ID.
 """
 
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-def get_github_id(username, password):
-    auth = HTTPBasicAuth(username, password)
+def get_github_id(username, token):
+    auth = HTTPBasicAuth(username, token)
     response = requests.get('https://api.github.com/user', auth=auth)
-    return response.json().get('id')
+
+    if response.status_code == 200:
+        return response.json().get('id')
+    else:
+        print(f"Error: {response.status_code}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: ./github_id.py <username> <access_token>")
+        print("Usage: ./github_id.py <username> <personal_access_token>")
         sys.exit(1)
 
     username = sys.argv[1]
-    password = sys.argv[2]
+    token = sys.argv[2]
 
-    github_id = get_github_id(username, password)
+    github_id = get_github_id(username, token)
     print(github_id)
