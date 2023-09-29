f1d886c952023a584a969f49b8ca826c662586df
userfiona <fionaramaesela@gmail.com>
2023-09-29 16:10:41 -0700

    add network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status and displays its content
-and the value of the X-Request-Id header using urllib.
-"""
+# Task 0. What's my status? #0
+import urllib.request
 
-if __name__ == "__main__":
-    import urllib.request
-    import sys
-
-    url = "https://alx-intranet.hbtn.io/status"
-
-    try:
-        with urllib.request.urlopen(url) as response:
-            data = response.read().decode('utf-8')
-
-        x_request_id = response.headers.get('X-Request-Id')
-        
-        print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data))
-        print("\t- X-Request-Id: {}".format(x_request_id))
-        
-    except urllib.error.URLError as e:
-        print("Error:", e.reason)
-
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
