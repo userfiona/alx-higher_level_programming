be135a430a839a4dc0882cb4af3b5d62c26d3061
userfiona <fionaramaesela@gmail.com>
2023-09-29 20:17:58 -0700

    add network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""Fetches https://alx-intranet.hbtn.io/status What's my status"""
 import requests
 
-if __name__ == '__main__':
-    url = "https://alx-intranet.hbtn.io/status"
 
-    try:
-        res = requests.get(url)
-        res.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
-
-        print("Body response:")
-        print("\t- type: {}".format(type(res.text)))
-        print("\t- content: {}".format(res.text))
-        except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
