c8f6758bd57e507d2c96ff3a525bcf3ba8488900
userfiona <fionaramaesela@gmail.com>
2023-09-29 19:48:20 -0700

    add network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """
-Lists 10 commits from the most recent to the oldest
-of the repository "rails" by the user "rails".
+ Time for an interview!
 """
-
-import sys
+from sys import argv
 import requests
 
-def list_recent_commits(repo_name, owner):
-    url = f'https://api.github.com/repos/{owner}/{repo_name}/commits'
-    response = requests.get(url)
-    commits = response.json()
-
-    for commit in commits[:10]:
-        sha = commit.get('sha')
-        author_name = commit.get('commit').get('author').get('name')
-        print(f'{sha}: {author_name}')
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
 
-    list_recent_commits(repo_name, owner)
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
