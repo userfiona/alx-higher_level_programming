1f92e4bc8a9155026d7fa810d75c6f3d75d741a1
userfiona <fionaramaesela@gmail.com>
2023-09-29 21:27:42 -0700

    add network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response Response header value
-"""
-import urllib.request
-import sys
+"""Python script that takes in a URL, sends a request
+to the URL and displays the value of the X-Request-Id
+variable found in the header of the response  Response header value"""
+
+from urllib import request
+from sys import argv
 
 if __name__ == "__main__":
-
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        value = response.info()
-    print(value['X-Request-Id'])
+    with request.urlopen(argv[1]) as page:
+        print(page.getheader("X-Request-Id"))
