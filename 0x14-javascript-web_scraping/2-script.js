7b3527116b93867c8904130371bb10d10cb7d65d
userfiona <fionaramaesela@gmail.com>
2023-10-24 16:33:59 -0700

    java

--- 0x14-javascript-web_scraping/2-script.js
+++ 0x14-javascript-web_scraping/2-script.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request(process.argv[2], function (_err, res) {
-  console.log('code:', res.statusCode); // Print the response status code if a response was received
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
