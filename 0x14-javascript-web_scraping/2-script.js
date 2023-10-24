762a774262f9ca69e3f1f4c4da4c3729fd1dffa9
userfiona <fionaramaesela@gmail.com>
2023-10-24 16:29:51 -0700

    java

--- 0x14-javascript-web_scraping/2-script.js
+++ 0x14-javascript-web_scraping/2-script.js
@@ -1,3 +1,7 @@
-$('DIV#red_header').click(function () {
-  $('HEADER').css({ color: '#FF0000' });
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
